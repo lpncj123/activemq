3cf10b779f25201ac4b50c02633430afb7c7ea72
lp <it_liup@163.com>
2023-04-03 18:36:23 +0800

    netty例子

---
+++ src/test/java/cn/lp/enumdemo/Weekday.java
@@ -0,0 +1,13 @@
+package cn.lp.enumdemo;
+
+/**
+ * @BelongsProject: activemq
+ * @BelongsPackage: cn.lp.enumdemo
+ * @Author: lp
+ * @CreateTime: 2023-04-03  16:11
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public enum Weekday {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
