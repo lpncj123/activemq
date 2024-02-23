e9e2efb62ba95a266b2a7315106ba583f0fc3a01
lp <it_liup@163.com>
2024-02-23 18:28:53 +0800

    更新

--- src/main/java/cn/lp/rocketmq/tranmsg/ICBCTransactionListener.java
+++ src/main/java/cn/lp/rocketmq/tranmsg/ICBCTransactionListener.java
@@ -6,6 +6,13 @@ import org.apache.rocketmq.client.producer.TransactionListener;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.common.message.MessageExt;
 
+/**
+ * @description:executeLocalTransaction 方法用于处理本地事务的执行和回滚逻辑，而 checkLocalTransaction 方法用于处理消息回查，确保 RocketMQ 能够获取到正确的事务状态。两者结合起来，可以保证 RocketMQ 的事务消息具有原子性。
+ * @author: lp
+ * @date: 2024/2/22 10:55
+ * @param:
+ * @return:
+ **/
 public class ICBCTransactionListener implements TransactionListener {
     // 回调操作方法
     // 消息预提交成功就会触发该方法的执行，用于完成本地事务
@@ -15,6 +22,7 @@ public class ICBCTransactionListener implements TransactionListener {
         // 假设接收到TAGA的消息就表示扣款操作成功，TAGB的消息表示扣款失败，
         // TAGC表示扣款结果不清楚，需要执行消息回查
         if (StringUtils.equals("TAGA", msg.getTags())) {
+//      在这里执行对数据库的操作，如果失败返回回滚。如果因为网络等问题原因没有获取到本地事务，调用下面方法进行回查。
             return LocalTransactionState.COMMIT_MESSAGE;
         } else if (StringUtils.equals("TAGB", msg.getTags())) {
             System.out.println("TAGB发送信息失败");
